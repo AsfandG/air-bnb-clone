066a19084dd84bf5b34ded3a6102ee1f522041c8
Asfandyar <asfandm17@gmail.com>
2024-03-20 08:15:31 +0500

    category UI completed

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,11 +1,23 @@
 "use client";
 import React from "react";
 import Container from "../Container";
-import { TbBeach } from "react-icons/tb";
-import { GiWindmill } from "react-icons/gi";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
+  GiIsland,
+  GiWindmill,
+} from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
+import { FaSkiing } from "react-icons/fa";
+import { BsSnow } from "react-icons/bs";
+import { IoDiamond } from "react-icons/io5";
 
 export const categories = [
   {
@@ -22,6 +34,66 @@ export const categories = [
     label: "Modern",
     icon: MdOutlineVilla,
     description: "This property is so modern!",
+  },
+  {
+    label: "Countryside",
+    icon: TbMountain,
+    description: "This property is in country side!",
+  },
+  {
+    label: "Pool",
+    icon: TbPool,
+    description: "This property is in country side!",
+  },
+  {
+    label: "Islands",
+    icon: GiIsland,
+    description: "This property is on island",
+  },
+  {
+    label: "Lake",
+    icon: GiBoatFishing,
+    description: "This property is close to Lake",
+  },
+  {
+    label: "Skiing",
+    icon: FaSkiing,
+    description: "This property has skiing activities",
+  },
+  {
+    label: "Castles",
+    icon: GiCastle,
+    description: "This property is in a castle",
+  },
+  {
+    label: "Camping",
+    icon: GiForestCamp,
+    description: "This property has camping activities",
+  },
+  {
+    label: "Arctic",
+    icon: BsSnow,
+    description: "This property is on island",
+  },
+  {
+    label: "Cave",
+    icon: GiCaveEntrance,
+    description: "This property is in cave",
+  },
+  {
+    label: "Desert",
+    icon: GiCactus,
+    description: "This property is in desert",
+  },
+  {
+    label: "Barn",
+    icon: GiBarn,
+    description: "This property is in barn",
+  },
+  {
+    label: "Lux",
+    icon: IoDiamond,
+    description: "This property is luxurious",
   },
 ];
 
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,17 +1,20 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Logo = () => {
+  const router = useRouter();
   return (
     <div>
       <Image
+        onClick={() => router.push("/")}
         src={"/images/logo.png"}
         width={100}
         height={100}
         priority
         alt="logo"
-        className="w-auto"
+        className="w-auto cursor-pointer"
       />
     </div>
   );
